7658ef0a2dba589805879a6418b991578e6a7b13
slothman3878 <sloth.man3878@gmail.com>
2021-04-30 17:56:26 -0400

    contract written

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ The idea behind it is simple:
 * When users "register" with an application, they are given an NFT which can later be used as proof of registration.
 * NFTs instead of generic fungible tokens since NFTs can store user meta-data (instead of asset URI)
 * The application authenticates the user (or in this case, the user's ethereum account) by checking token ownership.
+
+## Miscellaneous thoughts
+* It occurs to me that the User Meta Data doesn't have to be a struct. It can be, in fact, a contract, with mutable data. Something to think about.
+
+## TODOs
+1. non-abstract contract inheriting base NFTSubscriber contract
+2. Relevent tests
--- hardhat/contracts/NFTSubscriber.sol
+++ hardhat/contracts/NFTSubscriber.sol
@@ -1,7 +1,31 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SubscriptionToken is IERC721 {
+abstract contract SubscriptionToken is ERC721 {
+  using Strings for uint256;
+
+  mapping (uint256 => string) private _userMetaData; // Currently userMetaData 
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    return userMetaData(tokenId);
+  }
+
+  function userMetaData(uint256 tokenId) public view virtual returns (string memory) {
+    require(_exists(tokenId), "SubscriptionToken: Query for nonexistent token");
+    return _userMetaData[tokenId];
+  }
+
+  function _setTokenData(uint256 tokenId, string memory _userData) internal virtual {
+    require(_exists(tokenId), "SubscriptionToken: Data set of nonexistent token");
+    _userMetaData[tokenId] = _userData;
+  }
+
+  function _burn(uint256 tokenId) internal virtual override {
+    super._burn(tokenId);
+
+    if(bytes(_userMetaData[tokenId]).length != 0) {
+      delete _userMetaData[tokenId];
+    }
+  }
 }
